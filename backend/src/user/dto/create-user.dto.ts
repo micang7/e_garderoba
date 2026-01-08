35410fa2711af1bda989ee40279217f0490891c9
micang7 <mckinley@interia.pl>
2026-01-08 01:47:05 +0100

    feat(user): add User create dto

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -1,1 +1,44 @@
-export class CreateUserDto {}
+import {
+  IsString,
+  IsNotEmpty,
+  MaxLength,
+  MinLength,
+  IsEmail,
+  IsOptional,
+  Matches,
+  IsEnum,
+} from 'class-validator';
+import { UserRole } from '../../common/enums/user-role.enum';
+
+export class UserCreateDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(1)
+  @MaxLength(50)
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(1)
+  @MaxLength(50)
+  lastName: string;
+
+  @IsEmail({})
+  @IsNotEmpty()
+  @MaxLength(255)
+  email: string;
+
+  @IsOptional()
+  @MaxLength(20)
+  @Matches(/^[0-9+\-\s()]+$/)
+  phone?: string;
+
+  @IsEnum(UserRole)
+  @IsNotEmpty()
+  role: UserRole;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(1)
+  password: string;
+}
