1fd05d1e420c07ac5f41c2bd8c930ff38c2bd97a
micang7 <mckinley@interia.pl>
2026-01-08 05:04:49 +0100

    refactor(app): remove app service and controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 
 @Module({
@@ -24,7 +22,5 @@ import { UserModule } from './user/user.module';
     }),
     UserModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
