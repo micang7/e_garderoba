0698f391543a38e3b7130e7260c2408f5909f699
micang7 <mckinley@interia.pl>
2026-01-08 11:11:28 +0100

    feat(auth): add auth module using jwt

---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt/jwt.strategy';
+import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
+import { AuthController } from './auth.controller';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.getOrThrow('TOKEN_SECRET'),
+        signOptions: {
+          expiresIn: config.getOrThrow('TOKEN_EXPIRES_IN') ?? '1d',
+        },
+      }),
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
+  providers: [AuthService, JwtStrategy],
+  controllers: [AuthController],
+  exports: [AuthService],
+})
+export class AuthModule {}
