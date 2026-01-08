b5496ad4776018adcec23569d79f52787b5187bf
micang7 <mckinley@interia.pl>
2026-01-08 02:08:06 +0100

    feat(user): add User service create method

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,11 +1,41 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { plainToInstance } from 'class-transformer';
+
+import { User } from '../user/entities/user.entity';
+import { UserCreateDto } from '../user/dto/create-user.dto';
+import { UpdateUserDto } from '../user/dto/update-user.dto';
+import { UserDto } from './dto/user.dto';
+
+import { AlreadyExistsException } from '../common/exceptions';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private readonly users: Repository<User>,
+  ) {}
+
+  async create(dto: UserCreateDto): Promise<UserDto> {
+    if (await this.users.exists({ where: { email: dto.email } }))
+      throw new AlreadyExistsException('Email already exists');
+
+    const user = this.users.create({
+      firstName: dto.firstName,
+      lastName: dto.lastName,
+      email: dto.email,
+      phone: dto.phone,
+      role: dto.role,
+      passwordHash: await bcrypt.hash(dto.password, 12),
+    });
+
+    await this.users.save(user);
+
+    return plainToInstance(UserDto, user, {
+      excludeExtraneousValues: true,
+    }) as UserDto;
   }
 
   findAll() {
