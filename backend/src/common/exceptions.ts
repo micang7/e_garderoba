bc1545f1a1dd4277fad3f980a7e72aae01011925
micang7 <mckinley@interia.pl>
2026-01-08 03:30:31 +0100

    feat(exceptions): add not-found-exception

--- backend/src/common/exceptions.ts
+++ backend/src/common/exceptions.ts
@@ -1,5 +1,11 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
 
+export class NotFoundException extends HttpException {
+  constructor(message = 'Not found') {
+    super({ error: message }, HttpStatus.NOT_FOUND);
+  }
+}
+
 export class AlreadyExistsException extends HttpException {
   constructor(message = 'Already exists') {
     super({ error: message }, HttpStatus.CONFLICT);
