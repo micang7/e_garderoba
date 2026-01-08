0c102712f63ae2557d8838334585bcaa09462210
micang7 <mckinley@interia.pl>
2026-01-08 03:50:39 +0100

    feat(user): add User update dto

--- backend/src/user/dto/update-user.dto.ts
+++ backend/src/user/dto/update-user.dto.ts
@@ -1,1 +1,51 @@
-export class UpdateUserDto {}
+import {
+  IsString,
+  IsNotEmpty,
+  MaxLength,
+  MinLength,
+  IsEmail,
+  Matches,
+  IsEnum,
+  IsOptional,
+} from 'class-validator';
+import { UserRole } from '../../common/enums/user-role.enum';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+
+export class UserUpdateDto {
+  @ApiPropertyOptional({ example: '' })
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(1)
+  @MaxLength(50)
+  firstName?: string;
+
+  @ApiPropertyOptional({ example: '' })
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(1)
+  @MaxLength(50)
+  lastName?: string;
+
+  @ApiPropertyOptional({ example: '' })
+  @IsOptional()
+  @IsEmail()
+  @IsNotEmpty()
+  @MaxLength(255)
+  email?: string;
+
+  @ApiPropertyOptional({ example: '' })
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(20)
+  @Matches(/^[0-9+\-\s()]+$/)
+  phone?: string;
+
+  @ApiPropertyOptional({ example: '' })
+  @IsOptional()
+  @IsEnum(UserRole)
+  @IsNotEmpty()
+  role?: UserRole;
+}
