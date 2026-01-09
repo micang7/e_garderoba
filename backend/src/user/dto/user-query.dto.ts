84ed11d7dd840c9eee4f51efcb295b9d5d365fdb
micang7 <mckinley@interia.pl>
2026-01-09 23:12:32 +0100

    docs(user): add UserQueryDto docs

--- backend/src/user/dto/user-query.dto.ts
+++ backend/src/user/dto/user-query.dto.ts
@@ -7,50 +7,62 @@ import {
   IsDateString,
 } from 'class-validator';
 import { UserRole } from '../../common/enums/user-role.enum';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class UserQueryDto {
+  @ApiPropertyOptional({ example: '' })
   @IsOptional()
   @IsInt()
   @Min(0)
   offset?: number;
 
+  @ApiPropertyOptional({ example: '' })
   @IsOptional()
   @IsInt()
   @Min(1)
   limit?: number;
 
+  @ApiPropertyOptional({ example: '' })
   @IsOptional()
   @IsString()
   sort?: string;
 
+  @ApiPropertyOptional({ example: '' })
   @IsOptional()
   @IsString()
   order?: string;
 
+  @ApiPropertyOptional({ example: '' })
   @IsOptional()
   @IsString()
   search?: string;
 
+  @ApiPropertyOptional({ example: '' })
   @IsOptional()
   @IsString()
   firstName?: string;
 
+  @ApiPropertyOptional({ example: '' })
   @IsOptional()
   @IsString()
   lastName?: string;
 
+  @ApiPropertyOptional({ example: '' })
   @IsOptional()
   @IsString()
   email?: string;
 
+  @ApiPropertyOptional({ example: '' })
   @IsOptional()
   @IsEnum(UserRole)
   role?: UserRole;
 
+  @ApiPropertyOptional({ example: '' })
   @IsOptional()
   @IsDateString()
   createdFrom?: string;
 
+  @ApiPropertyOptional({ example: '' })
   @IsOptional()
   @IsDateString()
   createdTo?: string;
