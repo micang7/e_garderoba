b686c25d070d1fae5d6003a3071945c2e307bbb0
micang7 <mckinley@interia.pl>
2026-01-16 16:56:23 +0100

    feat(item): add ItemService.create

--- backend/src/item/item.service.ts
+++ backend/src/item/item.service.ts
@@ -1,11 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
 import { UpdateItemDto } from './dto/update-item.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Item } from './entities/item.entity';
+import { Repository } from 'typeorm';
+import { ItemDto } from './dto/item.dto';
+import { plainToInstance } from 'class-transformer';
 
 @Injectable()
 export class ItemService {
-  create(createItemDto: CreateItemDto) {
-    return 'This action adds a new item';
+  constructor(
+    @InjectRepository(Item)
+    private readonly items: Repository<Item>,
+  ) {}
+
+  async create(dto: CreateItemDto): Promise<ItemDto> {
+    if (await this.items.exists({ where: { code: dto.code } }))
+      throw new ConflictException('Item code already exists');
+
+    const item = this.items.create({
+      code: dto.code,
+      name: dto.name,
+      size: dto.size,
+      gender: dto.gender,
+      description: dto.description,
+    });
+
+    await this.items.save(item);
+
+    return plainToInstance(ItemDto, item, {
+      excludeExtraneousValues: true,
+    });
   }
 
   findAll() {
