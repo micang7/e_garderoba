628eb857572eab5e3a005f95aa560a05f8b0ce22
micang7 <mckinley@interia.pl>
2026-01-16 20:01:01 +0100

    feat(item): add endpoints jwt authorization

--- backend/src/item/item.controller.ts
+++ backend/src/item/item.controller.ts
@@ -8,24 +8,34 @@ import {
   Delete,
   HttpCode,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { ItemService } from './item.service';
 import { CreateItemDto } from './dto/create-item.dto';
 import { UpdateItemDto } from './dto/update-item.dto';
 import { ItemQueryDto } from './dto/item-query.dto';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { RolesGuard } from '../auth/jwt/guards/jwt-role.guard';
+import { JwtAuthGuard } from '../auth/jwt/guards/jwt-auth.guard';
+import { Role } from '../auth/jwt/decorators/jwt-role.decorator';
+import { UserRole } from '../common/enums/user-role.enum';
 
+@ApiBearerAuth('JWT')
 @Controller('items')
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class ItemController {
   constructor(private readonly itemService: ItemService) {}
 
   @Post()
   @HttpCode(201)
+  @Role(UserRole.Manager)
   async create(@Body() dto: CreateItemDto) {
     const data = await this.itemService.create(dto);
     return { data };
   }
 
   @Get()
+  @Role(UserRole.Manager)
   async findAll(@Query() query: ItemQueryDto) {
     const { data, total } = await this.itemService.findAll(query);
     return {
@@ -41,6 +51,7 @@ export class ItemController {
   }
 
   @Patch(':id')
+  @Role(UserRole.Manager)
   async update(@Param('id') id: number, @Body() dto: UpdateItemDto) {
     const data = await this.itemService.update(id, dto);
     return { data };
@@ -48,6 +59,7 @@ export class ItemController {
 
   @Delete(':id')
   @HttpCode(204)
+  @Role(UserRole.Manager)
   async delete(@Param('id') id: number) {
     await this.itemService.delete(id);
     return;
