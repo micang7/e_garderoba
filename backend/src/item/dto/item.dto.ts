2136d4e386b417c6baa35d4804ddfa27a0682a24
micang7 <mckinley@interia.pl>
2026-01-17 19:15:24 +0100

    docs: provide examples in return DTOs

--- backend/src/item/dto/item.dto.ts
+++ backend/src/item/dto/item.dto.ts
@@ -1,25 +1,36 @@
 import { Expose } from 'class-transformer';
 import { ItemGender } from '../enums/item-gender.enum';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class ItemDto {
+  @ApiProperty({ example: 1 })
   @Expose()
   id: number;
 
+  @ApiProperty({ example: 'RZE-M-SPD-1' })
   @Expose()
   code: string;
 
+  @ApiProperty({ example: 'Spodnie rzeszowskie' })
   @Expose()
   name: string;
 
+  @ApiProperty({ example: 'talia: 88 cm / biodra: 104 cm / nogawka: 82 cm' })
   @Expose()
   size?: string;
 
+  @ApiProperty({ enum: ItemGender, example: 'męski' })
   @Expose()
   gender?: ItemGender;
 
+  @ApiProperty({
+    example:
+      'Niebieskie spodnie z czerwonym herbem na zewnętrznej stronie nogawek.',
+  })
   @Expose()
   description?: string;
 
+  @ApiProperty({ example: '2026-01-17T14:34:11.876Z' })
   @Expose()
   createdAt: Date;
 }
