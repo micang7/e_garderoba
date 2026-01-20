44bde2bbbc136b256e4e2a13e4de1119b820d223
micang7 <mckinley@interia.pl>
2026-01-20 07:51:00 +0100

    fix: fix e2e test of filtering by date

--- backend/src/auth/dto/login-response.dto.ts
+++ backend/src/auth/dto/login-response.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { UserDto } from 'src/user/dto/user.dto';
+import { UserDto } from '../../user/dto/user.dto';
 
 class LoginResponsePayloadDto {
   @ApiProperty({ type: UserDto })
